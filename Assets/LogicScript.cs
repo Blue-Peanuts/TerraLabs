4a7dda4db71b4dd0a3109596635301f424162f75
Lilsofie <123223306+Lilsofie@users.noreply.github.com>
2023-01-21 15:32:33 -0500

    UI Side bar

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody2D rb;
+    public int MoveForce = 2;
+    public void Move_attach_bar()
+    {
+        
+    }
+
+}
