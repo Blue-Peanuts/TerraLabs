67f71a7d807bbfbc28d06e58c9ce88085c7a05a1
Sig <verusins.cn@gmail.com>
2023-01-20 23:49:03 -0500

    Sigmund Phase 1 final
    adding painting sections, left click to paint, right click to erase.

--- Assets/Sigmund/Scripts/BlockPlacer.cs
+++ Assets/Sigmund/Scripts/BlockPlacer.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class BlockPlacer : MonoBehaviour
 {
     public GameObject[] tilePrefabs; // the prefabs of selected tile
-    public int selectedTile = 0; // which tile are you currently using
+    public int selectedTile = 1; // which tile are you currently using
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // get world position
-            float Xfloored = Mathf.Floor(mousePosition.x);
-            float Yfloored = Mathf.Floor(mousePosition.y);
-            Vector3 snappedPosition = new Vector3(Xfloored + 0.5f, Yfloored + 0.5f, 0);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // get world position
+        float Xfloored = Mathf.Floor(mousePosition.x);
+        float Yfloored = Mathf.Floor(mousePosition.y);
+        Vector3 snappedPosition = new Vector3(Xfloored + 0.5f, Yfloored + 0.5f, 0);
 
-            GameObject[] Prefabs = GameObject.FindGameObjectsWithTag("Tiles");
+        GameObject[] Prefabs = GameObject.FindGameObjectsWithTag("Tiles");
+        if (Input.GetMouseButton(0))
+        {
             for (int i=0; i<Prefabs.Length; ++i)
             {
-                if (Prefabs[i].transform.position == snappedPosition) Destroy(Prefabs[i]);
+                if (Prefabs[i].transform.position == snappedPosition) Destroy(Prefabs[i]); // destroy overlapping
+            }
+            Instantiate(tilePrefabs[selectedTile], snappedPosition, Quaternion.identity);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            for (int i = 0; i < Prefabs.Length; ++i)
+            {
+                if (Prefabs[i].transform.position == snappedPosition) Destroy(Prefabs[i]); // destroy overlapping
             }
         }
     }
