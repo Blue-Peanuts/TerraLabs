17564265ec2af7e7d1392c73c82a3096effc6258
tjhiaj <j.tjhia@gmail.com>
2023-01-21 16:07:36 -0500

    Phase 2
    Created FocusedGiblet component (genetic data on-click for UI)

---
+++ Assets/Scripts/FocusedGiblet.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusedGiblet : MonoBehaviour
+{
+    public GameObject focusedGiblet;
+    public Genetics genetics;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                genetics = hit.collider.GetComponent<Genetics>();
+            }
+        }
+    }
+}
