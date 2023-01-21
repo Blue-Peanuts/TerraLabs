db8a364ebbdcd7376c3854d2e8da064eb23a3b6e
tjhiaj <j.tjhia@gmail.com>
2023-01-21 15:24:11 -0500

    Phase 1.5
    Implemented scroll wheel zoom and giblet blueprint.

--- Assets/CamMvmt.cs
+++ Assets/CamMvmt.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class CamMvmt : MonoBehaviour
 {
-    public float moveSpeed = 5f;
     Vector3 movement;
+    public float sensitivity = 10f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        fov = Camera.main.orthographicSize;
     }
+
+    float fov;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        fov -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, 5, 50);
+        Camera.main.orthographicSize = fov;
     }
 
     private void FixedUpdate()
     {
-        transform.position = transform.position + movement * moveSpeed * Time.fixedDeltaTime;
+        
+        transform.position = transform.position + movement * fov * Time.fixedDeltaTime;
     }
 }
