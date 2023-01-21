f31eb9977740cc3f57cc704c692def5f0f0c553e
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-01-21 01:01:30 -0500

    Fix UICameraRenderer warning

--- Assets/UICameraRenderer.cs
+++ Assets/UICameraRenderer.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class UICameraRenderer : MonoBehaviour
 {
-    [SerializeField] private Camera camera;
+    [SerializeField] private Camera targetCamera;
     private RawImage _rawImage;
     private RenderTexture _renderTexture;
     void Start()
     {
         _rawImage = GetComponent<RawImage>();
         _renderTexture = new RenderTexture(256, 256, 24);
-        camera.targetTexture = _renderTexture;
+        targetCamera.targetTexture = _renderTexture;
         _rawImage.texture = _renderTexture;
     }
 }
